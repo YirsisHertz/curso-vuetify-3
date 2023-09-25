310933b5393d09a2fd6e3c745cf3a6e245f88f8b
Yirsis Serrano <yirsishertz02@gmail.com>
2023-09-25 16:18:25 -0600

    vuetify course source code

---
+++ nuxt-vuetify/nuxt.config.ts
@@ -0,0 +1,20 @@
+export default defineNuxtConfig({
+  modules: ["@invictus.codes/nuxt-vuetify"],
+  vuetify: {
+    /* vuetify options */
+    vuetifyOptions: {
+      // @TODO: list all vuetify options
+    },
+
+    moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: true,
+      useIconCDN: true,
+
+      /* vite-plugin-vuetify options */
+      styles: true,
+      autoImport: true,
+      useVuetifyLabs: true,
+    },
+  },
+});
